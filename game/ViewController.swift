25f45c58f29e1890e89841b01002f389ea09ab33
Disukhi <ahmeddisukhi@gmail.com>
2024-03-13 08:45:29 -0400

    Updated pause after win

--- game/ViewController.swift
+++ game/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var moveit = -10 // blue
+    var moveit2 = 10 // red
     @IBOutlet weak var movableObject: UIView!
   
     @IBOutlet weak var movableobject2: UIView!
@@ -22,28 +23,38 @@ class ViewController: UIViewController {
     
     
     @IBAction func movement(_ sender: UIButton) {
-        let moveit = 10
+    
           
         
         
               
-        self.movableObject.transform = self.movableObject.transform.translatedBy(x: 0, y: -CGFloat(moveit))
+        self.movableObject.transform = self.movableObject.transform.translatedBy(x: 0, y: -CGFloat(moveit2))
         
         
         if self.movableObject.frame.minY <= self.labletoWIn.frame.maxY {
             self.labletoWIn.text = "WINNER IS RED"
+            moveit2 = 0
+            moveit = 0
         }
+        
+       
+        
+        
     }
     
     
     @IBAction func movement2(_ sender: UIButton) {
-        let moveit = -10
+  
         
         self.movableobject2.transform = self.movableobject2.transform.translatedBy(x: 0, y: -CGFloat(moveit))
         
         if self.movableobject2.frame.maxY >= self.labletoWIn.frame.minY {
             self.labletoWIn.text = "WINNER IS BLUE"
+            moveit = 0
+            moveit2 = 0
         }
+        
+        
     }
     
     
